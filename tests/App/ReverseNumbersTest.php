6ce724776b7a416e672b4b771520b84ed0274dd1
Liuver C. Espinosa <lc@mobadvcons.com>
2022-12-28 12:19:59 -0500

    update

--- tests/App/ReverseNumbersTest.php
+++ tests/App/ReverseNumbersTest.php
@@ -6,5 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class ReverseNumbersTest extends TestCase
 {
-
+    /** @test */
+    public function it_return_reverse_for_less_than_10()
+    {
+        $app = new ReverseNumbers;
+        $this->assertSame('0', $app->find_reverse_number('1'));
+    }
 }
